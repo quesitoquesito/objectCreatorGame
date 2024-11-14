66cc549b9c92a3b3e0e8388847e2d624422c1086
opa <carlosmoldef@gmail.com>
2024-11-14 00:31:34 +0000

    Optimización de código mediante arrays;

--- Assets/Scripts/SliderButtonsBehaviour.cs
+++ Assets/Scripts/SliderButtonsBehaviour.cs
@@ -7,39 +7,26 @@ public class SliderButtonsBehaviour : MonoBehaviour
     [SerializeField] GameObject[] objectsList;
     GameObject objectSelected;
     GameObject objectCreated;
-    string selectedItem;
+    int selectedItem;
     //Pop effect
     [SerializeField] float popAnimDuration;
     public void ShelfBags()
     {
-        selectedItem = "estantería";
-        SelectItem();
+        selectedItem = 0;
+        PlaceItem();
     }
     public void ShelfBoxes()
     {
-        selectedItem = "estantería2";
-        SelectItem();
-    }
-
-    //Seleccionar Item y Crearlo
-    void SelectItem()
-    {
-        Destroy(objectCreated); //DEBUG USE ONLY
-        if (selectedItem == "estantería")
-        {
-            objectSelected = objectsList[0];
-            objectSelected.transform.LeanScale(Vector3.zero, 0);
-        }
-        if (selectedItem == "estantería2")
-        {
-            objectSelected = objectsList[1];
-            objectSelected.transform.LeanScale(Vector3.zero, 0);
-        }
+        selectedItem = 1;
         PlaceItem();
     }
 
+    //Seleccionar Item y Crearlo
     void PlaceItem()
     {
+        Destroy(objectCreated); //DEBUG USE ONLY
+        objectSelected = objectsList[selectedItem];
+        objectSelected.transform.LeanScale(Vector3.zero, 0);
         objectCreated = Instantiate(objectSelected, Vector3.zero, Quaternion.identity);
         objectCreated.transform.LeanScale(new Vector3(1, 1, 1), popAnimDuration).setEase(LeanTweenType.easeOutBack);
     }
